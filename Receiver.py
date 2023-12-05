4ecb782b559e9b95d823e29718670ad0d05ee99a
ikenton <imkenton@cpp.edu>
2023-12-04 20:52:11 -0800

    renamed receiver

---
+++ Receiver.py
@@ -0,0 +1,6 @@
+# Cryptography Final Project
+# Russell Rickards
+# Isabela Kenton
+
+from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
--- cryptographyfp.py
+++
@@ -1,3 +0,0 @@
-# Cryptography Final Project
-# Russell Rickards
-# Isabela Kenton
