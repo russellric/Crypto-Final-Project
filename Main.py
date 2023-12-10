7b904f5d54102f4a340de69796508be38146ce84
ikenton <isabellakenton@gmail.com>
2023-12-09 16:57:06 -0800

    main is same as test now

--- Main.py
+++ Main.py
@@ -1,119 +1,85 @@
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
-from Crypto.PublicKey import RSA
 from Crypto.Hash import HMAC, SHA256
-import os
-
 
 def save_key_to_file(key, filename):
-        with open(filename, 'wb') as file:
-                file.write(key)
+    with open(filename, 'wb') as file:
+            file.write(key)
 
 def load_key_from_file(filename):
-        with open(filename, 'rb') as file:
-                key = file.read()
-        return key
-
-def encrypt_message(message, aes_key):
-        cipher = AES.new(aes_key, AES.MODE_EAX)
-        ciphertext, tag = cipher.encrypt_and_digest(message)
-        return ciphertext, tag
-
-def decrypt_message(ciphertext, tag, aes_key):
-        cipher = AES.new(aes_key, AES.MODE_EAX)
-        decrypted_message = cipher.decrypt_and_verify(ciphertext, tag)
-        return decrypted_message
-
-
-def verify_signature(message, signature, public_key):
-        h = HMAC.new(public_key, message,SHA256)
-        h.update(message)
-        try:
-                h.verify(signature)
-                return True
-        except ValueError:
-                return False
-        
+    with open(filename, 'rb') as file:
+            key = file.read()
+    return key
 
 def generate_keys():
-        # Step 1: Generate RSA key pair
-        key = RSA.generate(2048)
-        private_key = key.export_key()
-        public_key = key.publickey().export_key()
-        save_key_to_file(public_key, "public_key_receiver.pem")
-        save_key_to_file(private_key, "private_key_receiver.pem")
-        
-        key = RSA.generate(2048)
-        private_key = key.export_key()
-        public_key = key.publickey().export_key()
-        save_key_to_file(private_key, 'private_key_sender.pem')
-        save_key_to_file(public_key, 'public_key_sender.pem')
+    # Step 1: Generate RSA key pair
+    #RECEIVER KEYS
+    key = RSA.generate(2048)
+    private_key = key.export_key()
+    public_key = key.publickey().export_key()
+    save_key_to_file(public_key, "public_key_receiver.pem")
+    save_key_to_file(private_key, "private_key_receiver.pem")
 
-    
+    #SENDER KEYS
+    key = RSA.generate(2048)
+    private_key = key.export_key()
+    public_key = key.publickey().export_key()
+    save_key_to_file(private_key, 'private_key_sender.pem')
+    save_key_to_file(public_key, 'public_key_sender.pem')
 
 def sender():
-        # Step 2: Load public key of the receiver
-        public_key_receiver = RSA.import_key(load_key_from_file('public_key_receiver.pem'))
-        private_key_sender = RSA.import_key(load_key_from_file("private_key_sender.pem"))
-        public_key_sender = RSA.import_key(load_key_from_file("public_key_sender.pem"))
+    #open the message file
+    with open('message.txt', 'rb') as file:
+        message = file.read()
+    data = message
+    
+    #sender knows and uses these keys
+    recipient_key = RSA.import_key(load_key_from_file("public_key_receiver.pem"))
+    private_key_sender = RSA.import_key(load_key_from_file("private_key_sender.pem"))
+    aes_key = get_random_bytes(16)
 
-    # Step 3: Generate AES key
-        aes_key = get_random_bytes(16)  # 128-bit key for AES
+    # Encrypt the aes key with the public RSA key
+    cipher_rsa = PKCS1_OAEP.new(recipient_key)
+    enc_aes_key = cipher_rsa.encrypt(aes_key)
 
-    # Step 4: Read the message from file
-        with open('message.txt', 'rb') as file:
-                message = file.read()
+    # Encrypt the data with the AES aes key
+    cipher_aes = AES.new(aes_key, AES.MODE_EAX)
+    ciphertext, tag = cipher_aes.encrypt_and_digest(data)
 
-    # Step 5: Encrypt the message with AES
-        cipher = AES.new(aes_key, AES.MODE_EAX)
-        ciphertext, tag = cipher.encrypt_and_digest(message)
-        
+    #hmac signature
+    h = HMAC.new(aes_key, digestmod=SHA256)
+    h.update(message)
 
-    # Step 6: Encrypt the AES key with the receiver's public key
-        rsa_cipher = PKCS1_OAEP.new(public_key_receiver)
-        encrypted_aes_key = rsa_cipher.encrypt(aes_key)
+    # write to file
+    #TODO: ADD HMAC TO FILE
+    with open("Transmitted_Data", "wb") as file:
+        file.write(enc_aes_key)
+        file.write(cipher_aes.nonce)
+        file.write(tag)
+        file.write(ciphertext)
 
-    # Step 7: Generate Message Authentication Code (MAC)
-        #signature = HMAC.new(aes_key, ciphertext, digestmod=SHA256).digest()
-        
-
-    # Step 8: Write the transmitted data to a file
-        with open('Transmitted_Data', 'wb') as file:
-                file.write(ciphertext)
-                file.write(tag)
-                file.write(encrypted_aes_key)
-                #file.write(signature)
+    print("Encrypted")
 
 def receiver():
-        public_key_receiver = RSA.import_key(load_key_from_file('public_key_receiver.pem'))
-        public_key_sender = RSA.import_key(load_key_from_file("public_key_sender.pem"))
-        private_key_receiver = RSA.import_key(load_key_from_file("private_key_receiver.pem"))
-        # Step 2: Read transmitted data from file
-               
-        with open('Transmitted_Data', 'rb') as file:
-                ciphertext = file.read(16)
-                tag = file.read(16)
-                encrypted_aes_key = file.read(256)
-                #signature = file.read(32)
-                
-        # Step 3: Decrypt the AES key with the receiver's rsa public key
-        rsa_cipher = PKCS1_OAEP.new(private_key_receiver)
-        aes_key = rsa_cipher.decrypt(encrypted_aes_key)
+    file_in = open("Transmitted_Data", "rb")
 
-        # Step 4: Verify the signature
-        #mac_check = HMAC.new(aes_key, ciphertext, SHA256).digest()
-        #if signature != mac_check:
-        #        print("MAC does not match")
+    private_key = RSA.import_key(load_key_from_file("private_key_receiver.pem"))
 
+    #TODO: GET HMAC FROM FILE
+    enc_aes_key, nonce, tag, ciphertext = \
+    [ file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
+    file_in.close()
 
-        # Step 5: Decrypt the message with AES
-        decrypted_message = decrypt_message(ciphertext, tag, aes_key)
-        print("Decrypted Message:", decrypted_message)
+    # Decrypt the aes key with the private RSA key
+    cipher_rsa = PKCS1_OAEP.new(private_key)
+    aes_key = cipher_rsa.decrypt(enc_aes_key)
 
+    # Decrypt the data with the AES aes key
+    cipher_aes = AES.new(aes_key, AES.MODE_EAX, nonce)
+    message = cipher_aes.decrypt_and_verify(ciphertext, tag)
+    print(message.decode("utf-8"))
 
 generate_keys()
-# Run sender first to generate keys and transmit data
 sender()
-# Run receiver to receive and decrypt the transmitted data
 receiver()
